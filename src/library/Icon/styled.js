317b2397b376dfb989b3e89a97be147816e427f0
Michael Martinez <michael.martinez@targetx.com>
2019-05-22 12:53:54 -0700

    Update version 1.40.0

--- src/library/Icon/styled.js
+++ src/library/Icon/styled.js
@@ -6,7 +6,30 @@ import { iconTheme } from './themes';
 export const IconRoot = styled('svg', {
   shouldForwardProp: (prop) =>
     ['color', 'size'].indexOf(prop) === -1 && isPropValid(prop)
-})(({ color, rtl, size, theme: baseTheme }) => {
+})(({
+    /* TargetX Custom Styles */
+    bottom,
+    clickable,
+    cursor,
+    left,
+    margin,
+    marginBottom,
+    marginHorizontal,
+    marginLeft,
+    marginRight,
+    marginTop,
+    marginVertical,
+    position,
+    right,
+    top,
+    verticalAlign,
+
+    /* Built-In Styles */
+    color,
+    rtl,
+    size,
+    theme: baseTheme
+ }) => {
   let theme = iconTheme(baseTheme);
 
   return {
@@ -15,6 +38,29 @@ export const IconRoot = styled('svg', {
     fontSize: theme.fontSize_base,
     height: theme[`Icon_size_${size}`] || size,
     transform: theme.direction === 'rtl' && rtl && 'scaleX(-1)',
-    width: theme[`Icon_size_${size}`] || size
+    width: theme[`Icon_size_${size}`] || size,
+
+    /* TargetX Custom Styles */
+    ...(bottom && { bottom }),
+    ...(clickable ? { cursor: 'pointer' } : {}),
+    ...(cursor ? { cursor } : {}),
+    ...(left && { left }),
+    ...(margin && { margin }),
+    ...(marginBottom && { marginBottom }),
+    ...(marginHorizontal && {
+      marginLeft: marginHorizontal,
+      marginRight: marginHorizontal
+    }),
+    ...(marginLeft && { marginLeft }),
+    ...(marginRight && { marginRight }),
+    ...(marginTop && { marginTop }),
+    ...(marginVertical && {
+      marginBottom: marginVertical,
+      marginTop: marginVertical
+    }),
+    ...(position && { position }),
+    ...(right && { right }),
+    ...(top && { top }),
+    ...(verticalAlign && { verticalAlign })
   };
 });
