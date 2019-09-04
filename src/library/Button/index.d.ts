0a5936b6a5ec472f2234bb6c9cef26e61269da50
Steven Cummings <steven.cummings@targetx.com>
2019-09-04 10:41:59 -0700

    Update src/library/Button/index.d

--- src/library/Button/index.d.ts
+++ src/library/Button/index.d.ts
@@ -1,1 +1,29 @@
-declare module '@targetx/mineral-ui/Button';
+import { ElementType, ReactElement, ReactNode, SyntheticEvent } from 'react';
+
+type Size = 'small' | 'medium' | 'large' | 'jumbo';
+type Variant = 'danger' | 'grayscale' | 'success' | 'warning';
+
+export declare namespace Button {
+  export interface Props {
+    as?: ElementType;
+    children?: ReactNode;
+    circular?: boolean;
+    css?: { [key: string]: any };
+    disabled?: boolean;
+    fullWidth?: boolean;
+    href?: string;
+    iconEnd?: ReactElement;
+    iconStart?: ReactElement;
+    justifyContent?: string;
+    minimal?: boolean;
+    onClick?: (event: SyntheticEvent) => void;
+    primary?: boolean;
+    size?: Size;
+    type?: string;
+    variant?: Variant;
+    width?: string;
+  }
+}
+
+export declare function Button(props: Button.Props): ReactElement;
+export default Button;
